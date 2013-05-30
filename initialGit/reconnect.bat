a6421e9c5311798da765a1f93498dc520e4b1a04
stephan-dowding <stephan.dowding@googlemail.com>
2013-05-30 10:35:01 +0100

    using java for reconnect script

--- initialGit/reconnect.bat
+++ initialGit/reconnect.bat
@@ -1,1 +1,5 @@
-node reconnecter.js
+set branch=
+for /f "delims=" %%a in ('java -classpath branchFinder/org.eclipse.jgit-2.3.1.201302201838-r.jar:branchFinder/ branchFinder.Finder') do @set branch=%%a
+
+git pull
+git reset --hard origin/%branch%
--- initialGit/reconnect.sh
+++ initialGit/reconnect.sh
@@ -1,1 +1,3 @@
-node reconnecter.js
+branch=$(java -classpath branchFinder/org.eclipse.jgit-2.3.1.201302201838-r.jar:branchFinder/ branchFinder.Finder)
+git pull
+git reset --hard origin/$branch

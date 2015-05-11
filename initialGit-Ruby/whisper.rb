b0b4dfbfdefb6f2211519fe3688ba19f7702b216
Stephan Dowding <stephan.dowding@gmail.com>
2015-05-11 22:43:16 +0800

    Adding the server ip address as command line argument

--- initialGit-Ruby/whisper.rb
+++ initialGit-Ruby/whisper.rb
@@ -1,13 +1,18 @@
 require 'net/http'
 require 'json'
 
+if ARGV[0] == nil
+  puts 'PROPER USAGE: ruby whisper.rb <Question Server IP address>'
+  exit 1
+end
+
 git_branch = `git symbolic-ref --short HEAD`.strip!
-uri = URI("http://127.0.0.1:3000/challenge/question/#{git_branch}")
+uri = URI("http://#{ARGV[0]}:3000/challenge/question/#{git_branch}")
 json = JSON.parse(Net::HTTP.get(uri))
 
 the_end = json['start'].to_i
 
-uri = URI("http://127.0.0.1:3000/challenge/answer/#{git_branch}")
+uri = URI("http://#{ARGV[0]}:3000/challenge/answer/#{git_branch}")
 response = Net::HTTP.post_form(uri, 'end' => the_end)
 
 if response.kind_of? Net::HTTPSuccess

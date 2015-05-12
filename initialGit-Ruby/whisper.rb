3572ebf7231022ed39fc4ea96349593e98d87f89
Stephan Dowding <stephan.dowding@gmail.com>
2015-05-12 23:38:34 +0800

    Making initial whisper.rb understand the multiple requests

--- initialGit-Ruby/whisper.rb
+++ initialGit-Ruby/whisper.rb
@@ -6,17 +6,24 @@ if ARGV[0] == nil
   exit 1
 end
 
-git_branch = `git symbolic-ref --short HEAD`.strip!
-uri = URI("http://#{ARGV[0]}:3000/challenge/question/#{git_branch}")
-json = JSON.parse(Net::HTTP.get(uri))
+def doChallenge(git_branch)
+  uri = URI("http://#{ARGV[0]}:3000/challenge/question/#{git_branch}")
+  json = JSON.parse(Net::HTTP.get(uri))
 
-the_end = json['start'].to_i
+  the_end = json['start'].to_i
 
-uri = URI("http://#{ARGV[0]}:3000/challenge/answer/#{git_branch}")
-response = Net::HTTP.post_form(uri, 'end' => the_end)
+  uri = URI("http://#{ARGV[0]}:3000/challenge/answer/#{git_branch}")
+  response = Net::HTTP.post_form(uri, 'end' => the_end)
 
-if response.kind_of? Net::HTTPSuccess
-  puts 'Yay!! got it right! =D'
-else
-  puts 'Boo! Wrong! :('
+  if response.kind_of? Net::HTTPSuccess
+    puts 'Yay!! got it right! =D'
+  elsif response.kind_of? Net::HTTPRedirection
+    puts 'Got it right, but do another...'
+    doChallenge(git_branch)
+  else
+    puts 'Boo! Wrong! :('
+  end
 end
+
+git_branch = `git symbolic-ref --short HEAD`.strip!
+doChallenge git_branch

de8c2d4333bd0a2c4b6590945b9984dc59ca688c
Florian Sellmayr <florian.sellmayr@gmail.com>
2022-11-12 12:02:57 +0700

    make port configurable in docker-composes and expose port 80 instead of 8888 for easier consumption when deploying to real infrastructure

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -2,5 +2,5 @@ locals {
   host = var.route_53_zone_id != "" ? aws_route53_record.app[0].fqdn : aws_instance.app.public_ip
 }
 output "app_address" {
-  value = "http://${local.host}:8888"
+  value = "http://${local.host}"
 }
